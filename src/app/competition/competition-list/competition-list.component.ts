4f0474a1dfe652f708f1b65d96a2cc0c52dfa50f
Roland Seidel <register@interpolar.ch>
2017-01-18 23:45:12 +0100

    init competition-list

---
+++ src/app/competition/competition-list/competition-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gymapp-competition-list',
+  templateUrl: './competition-list.component.html',
+  styleUrls: ['./competition-list.component.scss']
+})
+export class CompetitionListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
