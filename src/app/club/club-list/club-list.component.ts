8123339bfcefcfb59e57175e0e53fd1b5f8be909
Roland Seidel <register@interpolar.ch>
2017-01-29 13:08:12 +0100

    refactor club-list with abstract-list.component

--- src/app/club/club-list/club-list.component.ts
+++ src/app/club/club-list/club-list.component.ts
@@ -1,117 +1,29 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
-import { Subscription } from 'rxjs/Subscription';
-import { Subject } from 'rxjs/Subject';
-import 'rxjs/add/operator/combineLatest';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../app-state.reducer';
 import { Club } from '../../model/backend-typings';
 import * as fromRoot from '../../app-state.reducer';
 import * as fromClubs from '../club.actions';
+import { AbstractListComponent } from '../../shared/abstract-list.component';
 
 @Component({
   selector: 'gymapp-club-list',
   templateUrl: './club-list.component.html',
   styleUrls: ['./club-list.component.scss']
 })
-export class ClubListComponent implements OnInit, OnDestroy {
-  clubs: Club[];
-  clubKinds: string[] = [];
-  kindmap = {};
+export class ClubListComponent extends AbstractListComponent<Club> implements OnInit {
   isClubloading: Observable<boolean>;
-  selectedTabSubject = new Subject<string>();
-  selectedTab: string;
-  subs: Subscription[] = [];
   message = 'Clubs loading ...';
 
-  constructor(private store: Store<AppState>) { }
+  constructor(protected store: Store<AppState>) {
+    super(store);
+  }
 
   ngOnInit() {
-    this.subs.push(this.selectedTabSubject.subscribe(pred => this.selectedTab = pred));
-
     this.store.dispatch(fromClubs.loadAllAction());
-
-    const unfilteredClubs = this.store.select(fromRoot.getClubs);
-    this.subs.push(unfilteredClubs.subscribe((clubs) => {
-      this.kindmap = {};
-      clubs.forEach(club => club.kind.forEach(kind => {
-        let kindcnt = this.kindmap[kind];
-        if (kindcnt === undefined) {
-          kindcnt = 1;
-        } else {
-          kindcnt++;
-        }
-        this.kindmap[kind] = kindcnt;
-      }));
-      const allkinds = Object.keys(this.kindmap)
-        .map(key => Object.assign({kind: key, cnt: this.kindmap[key], }))
-        .sort((a, b) => a.cnt - b.cnt);
-      this.clubKinds = ['All', ...allkinds.slice(0, 1).map(kind => kind.kind), 'Other'];
-    }));
-
-    this.subs.push(Observable.combineLatest(
-      unfilteredClubs, this.selectedTabSubject,
-      (clubs, predicate) => {
-        const ret = clubs.filter(club => this.filter(club, predicate));
-        return ret;
-      }
-    ).subscribe(clubs => this.clubs = clubs));
-
+    const maxTabs = 6;
+    this.connect(fromRoot.getClubs, maxTabs, (club) => club.kind);
     this.isClubloading = this.store.select(fromRoot.isLoadingClub);
-    this.selectedTabSubject.next('All');
-  }
-
-  ngOnDestroy() {
-    this.subs.forEach(sub => sub.unsubscribe);
-  }
-
-  select(kind) {
-    this.selectedTabSubject.next(kind);
-  }
-
-  getKindCount(kind: string) {
-    switch (kind) {
-      case 'All':
-        return Object.keys(this.kindmap)
-          .map(k => this.kindmap[k])
-          .reduce((a, b) => a + b);
-
-      case 'Other':
-        return Object.keys(this.kindmap)
-          .filter(k => this.clubKinds.indexOf(k) > -1)
-          .map(k => this.kindmap[k])
-          .reduce((a, b) => a + b);
-
-      default: {
-        return this.kindmap[kind];
-      }
-    }
-  }
-
-  isSelected(kind) {
-    if (!this.clubKinds) {
-      return false;
-    }
-    return this.selectedTab === kind;
-  }
-
-  private filter(club: Club, kind: string): boolean {
-    if (!this.clubKinds) {
-      return true;
-    }
-    switch (kind) {
-      case 'All':
-        return true;
-
-      case 'Other':
-        return club.kind
-          .filter(k => this.clubKinds.indexOf(k) === -1)
-          .length > 0;
-
-      default:
-        return club.kind
-          .filter(k => kind === k)
-          .length > 0;
-    }
   }
 }
---
+++ src/app/shared/abstract-list.component.ts
@@ -0,0 +1,122 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+import { Subscription } from 'rxjs/Subscription';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/combineLatest';
+import { Store } from '@ngrx/store';
+import { AppState } from '../app-state.reducer';
+import * as fromRoot from '../app-state.reducer';
+import { Selector } from 'reselect';
+
+export const OTHER_KIND = 'Other';
+export const ALL_KINDS = 'All';
+
+export class AbstractListComponent<T> implements OnDestroy {
+
+  protected subscriptions: Subscription[] = [];
+
+  private items: T[];
+  private kindItems: string[] = [];
+  private selectedTabSubject = new Subject<string>();
+  private selectedTab: string;
+  private allKindsMap = {};
+  private kindsMap = {};
+  private itemExtractor: (T) => string[];
+
+  constructor(protected store: Store<AppState>) { }
+
+  protected connect(selector: Selector<AppState, T[]>, maxTabs: number, extractor: (T) => string[]) {
+    this.itemExtractor = extractor;
+    this.subscriptions.push(this.selectedTabSubject.subscribe(predicate => this.selectedTab = predicate));
+
+    const unfilteredItems = this.store.select(selector);
+    this.subscriptions.push(unfilteredItems.subscribe((items) => {
+      this.allKindsMap = {};
+      items.forEach(item => extractor(item).forEach(kind => {
+        let kindcnt = this.allKindsMap[kind];
+        if (kindcnt === undefined) {
+          kindcnt = 1;
+        } else {
+          kindcnt++;
+        }
+        this.allKindsMap[kind] = kindcnt;
+      }));
+      const allkinds = Object.keys(this.allKindsMap)
+        .map(key => Object.assign({kind: key, cnt: this.allKindsMap[key], }))
+        .sort((a, b) => a.cnt - b.cnt);
+      this.kindItems = [ALL_KINDS, ...allkinds.slice(0, maxTabs).map(kind => kind.kind), OTHER_KIND];
+      this.kindItems.forEach(item => {
+        this.kindsMap[item] = this.getKindCount(item);
+      });
+    }));
+
+    this.subscriptions.push(Observable.combineLatest(
+      unfilteredItems, this.selectedTabSubject,
+      (items, predicate) => {
+        const ret = items.filter(item => this.filter(item, predicate));
+        return ret;
+      }
+    ).subscribe(items => this.items = items));
+
+    this.selectedTabSubject.next(ALL_KINDS);
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(sub => sub.unsubscribe);
+  }
+
+  select(kind: string) {
+    this.selectedTabSubject.next(kind);
+  }
+
+  private getKindCount(kind: string) {
+    if (this.kindsMap[kind]) {
+      return this.kindsMap[kind];
+    }
+    switch (kind) {
+      case ALL_KINDS:
+        return Object.keys(this.allKindsMap)
+          .map(k => this.allKindsMap[k])
+          .concat([0])
+          .reduce((a, b) => a + b);
+
+      case OTHER_KIND:
+        return Object.keys(this.allKindsMap)
+          .filter(k => this.kindItems.indexOf(k) === -1)
+          .map(k => this.allKindsMap[k])
+          .concat([0])
+          .reduce((a, b) => a + b);
+
+      default: {
+        return this.allKindsMap[kind];
+      }
+    }
+  }
+
+  public isSelected(kind: string) {
+    if (!this.kindItems) {
+      return false;
+    }
+    return this.selectedTab === kind;
+  }
+
+  private filter(item: T, kind: string): boolean {
+    if (!this.kindItems) {
+      return true;
+    }
+    switch (kind) {
+      case ALL_KINDS:
+        return true;
+
+      case OTHER_KIND:
+        return this.itemExtractor(item)
+          .filter(k => this.kindItems.indexOf(k) === -1)
+          .length > 0;
+
+      default:
+        return this.itemExtractor(item)
+          .filter(k => kind === k)
+          .length > 0;
+    }
+  }
+}
