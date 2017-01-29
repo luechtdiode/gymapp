748872c0c5350b476082456318ca93e81467c0c9
Roland Seidel <register@interpolar.ch>
2017-01-29 13:08:34 +0100

    create competition-list feature

--- src/app/competition/competition-list/competition-list.component.ts
+++ src/app/competition/competition-list/competition-list.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractListComponent } from '../../shared/abstract-list.component';
+import { Observable } from 'rxjs/Rx';
+import { Competition } from '../../model/backend-typings';
+import * as fromRoot from '../../app-state.reducer';
+import * as fromCompetitions from '../competition.actions';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../app-state.reducer';
 
 @Component({
   selector: 'gymapp-competition-list',
   templateUrl: './competition-list.component.html',
   styleUrls: ['./competition-list.component.scss']
 })
-export class CompetitionListComponent implements OnInit {
+export class CompetitionListComponent extends AbstractListComponent<Competition> implements OnInit {
+  isClubloading: Observable<boolean>;
+  message = 'Clubs loading ...';
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(protected store: Store<AppState>) {
+    super(store);
   }
 
+  ngOnInit() {
+    this.store.dispatch(fromCompetitions.loadAllAction());
+    const maxTabs = 6;
+    this.connect(fromRoot.getCompetitions, maxTabs, (competition: Competition) => [competition.kind]);
+    this.isClubloading = this.store.select(fromRoot.isLoadingClub);
+  }
 }
