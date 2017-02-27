fafed7d7a2b5b600f85cd4a47fd602d0e10df208
Roland Seidel <register@interpolar.ch>
2017-02-27 21:37:43 +0100

    try fix karma-problems

--- karma.conf.js
+++ karma.conf.js
@@ -9,9 +9,9 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('@angular/cli/plugins/karma'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter')
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular/cli/plugins/karma')
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -35,7 +35,7 @@ module.exports = function (config) {
     reporters: config.angularCli && config.angularCli.codeCoverage ?
           ['progress', 'coverage-istanbul'] :
           ['progress', 'kjhtml'],
-    port: 9876,
+    port: 9877,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
