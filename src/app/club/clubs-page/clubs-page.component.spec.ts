fb1c9f8ebde7d1fb949c6043aaf23b2490f23674
Roland Seidel <register@interpolar.ch>
2017-03-02 23:12:20 +0100

    add more tests

--- src/app/club/clubs-page/clubs-page.component.spec.ts
+++ src/app/club/clubs-page/clubs-page.component.spec.ts
@@ -2,16 +2,47 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { Store, Action, StoreModule } from '@ngrx/store';
+import { AppState } from '../../app-state.reducer';
+import { reducer } from '../../app-state.reducer';
+import { Observable } from 'rxjs/Observable';
 
 import { ClubsPageComponent } from './clubs-page.component';
+import { ClubListComponent } from '../club-list/club-list.component';
+import { ClubMediaComponent } from '../club-media/club-media.component';
+import { Club } from '../../model/backend-typings';
 
 describe('ClubsPageComponent', () => {
   let component: ClubsPageComponent;
   let fixture: ComponentFixture<ClubsPageComponent>;
+  const clubListStub: Club[] = [
+    <Club>{
+      _id: 'testId',
+      name: 'Changed-Clubname',
+      image: 'images/club.png',
+      homepage: '',
+      kind: ['uio']
+    }
+  ];
+  const storeStub: Store<AppState> = <Store<AppState>> {
+        select: (selector: any, ...paths: string[]) => {
+          console.log('selecting ', selector);
+          return Observable.of(clubListStub);
+        },
+        dispatch: (action: Action) => {
+          console.log('dispatching ', action);
+        }
+    };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ClubsPageComponent ]
+      declarations: [ ClubsPageComponent, ClubListComponent, ClubMediaComponent ],
+      providers: [
+        {provide: Store, useValue: storeStub}
+      ],
+      imports: [
+        StoreModule.provideStore({reducer})
+      ]
     })
     .compileComponents();
   }));
@@ -25,4 +56,5 @@ describe('ClubsPageComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
--- src/app/sponsor/sponsor.service.spec.ts
+++ src/app/sponsor/sponsor.service.spec.ts
@@ -2,11 +2,41 @@
 
 import { TestBed, async, inject } from '@angular/core/testing';
 import { SponsorService } from './sponsor.service';
+import { Sponsor } from '../model/backend-typings';
+import { Observable } from 'rxjs/Rx';
+import { CrudService } from '../shared/crud.service';
+
 
 describe('SponsorService', () => {
+  const sponsorListStub: Sponsor[] = [
+    <Sponsor>{
+      _id: 'testId',
+      name: 'Changed-Sponsorname',
+      image: 'images/sponsor.png',
+      homepage: '',
+      kind: ['uio'],
+    },
+  ];
+
+  const crudStub: CrudService = <CrudService>{
+    unsave: () => crudStub,
+    authenticated: () => false,
+    post: (url: string, loginData) => {},
+    get: (url: string) => Observable.of(sponsorListStub)
+  };
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        {provide: SponsorService, useValue: new SponsorService(crudStub)},
+      ],
+    })
+    .compileComponents();
+  }));
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [SponsorService]
+      providers: [SponsorService],
     });
   });
 
