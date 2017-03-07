8af452da2bbd7bc859b4535722eab3563d35e7ac
Roland Seidel <register@interpolar.ch>
2017-03-07 23:45:07 +0100

    code cleanups

--- src/app/shared/auth.service.spec.ts
+++ src/app/shared/auth.service.spec.ts
@@ -2,6 +2,7 @@
 
 import { AuthService } from './auth.service';
 import { CrudService } from './crud.service';
+import { Observable } from 'rxjs/Observable';
 
 describe('AuthService', () => {
   const crudStub: CrudService = <CrudService>{
@@ -13,16 +14,18 @@ describe('AuthService', () => {
   const service = new AuthService(crudStub);
 
   it('should register new user...', () => {
-    expect(service).toBeTruthy();
-    let spy = spyOn(crudStub, 'post');
+    const spy = spyOn(crudStub, 'post');
     service.register({name: 'Hans'});
     expect(spy.calls.any()).toBeTruthy();
   });
 
   it('should login a user...', () => {
-    expect(service).toBeTruthy();
-    let spy = spyOn(crudStub, 'post');
+    const spy = spyOn(crudStub, 'post');
     service.register({password: 'Hans'});
     expect(spy.calls.any()).toBeTruthy();
+  });
+
+  it('should logout a user...', () => {
+    expect(service.logout()).toEqual(Observable.of(true));
   });
 });
